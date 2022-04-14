05d3f23247467fe9c38fcb6f406e8b2e46be3ee5
lysiandon <don.lysiane@gmail.com>
2022-04-14 11:46:28 +0200

    set up backend

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,18 @@ mongoose
 app.use(express.json());
 
 //--------------- ROUTERS ------------------//
+const contactRouter = require("/routers/contactRouter")
+const questionnaireRouter = require("/routers/questionnaireRouter")
+const dashboarRouter = require("/routers/dashboardRouter")
+const loginRouter = require("/routers/loginRouter")
+const logoutRouter = require("/routers/logoutRouter")
 
+//--------------- ROUTES -------------------//
+app.use("/login", loginRouter);
+app.use("/logout", logoutRouter);
+app.use("/contact", contactRouter);
+app.use("/questionnaire", questionnaireRouter);
+app.use('/dashboard', dashboardRouter);
 //---------------- ROUTES -----------------//
 app.get("*", (_req, res) => {
   res.status(404).send("Error 404 - Not found");
@@ -28,3 +39,6 @@ app.get("*", (_req, res) => {
 app.listen(port, () => {
   console.log(`Server listening at : http://localhost:${port}`);
 });
+
+
+
