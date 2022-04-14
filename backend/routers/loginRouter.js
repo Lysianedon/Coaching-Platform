d388006aec5a109525416d87596fe1331320f05f
jess <jessica.elessa@gmail.com>
2022-04-14 22:20:11 +0200

    changed loginRouter.js

--- backend/routers/loginRouter.js
+++ backend/routers/loginRouter.js
@@ -3,11 +3,39 @@ const express = require("express");
 const router = express.Router();
 
 //----------- MIDDLEWARE ------------//
+router.use(cookieParser());
 
-// const auth = require("../utils/middlewares/authentification")
+//------------ MODEL -----------------//
+const User = require("../models/userModel");
 
 //------------- ROUTE ---------------//
-// router.post("/", check mail et pass, checkCookie, checkRequest, async (req, res) => {
-// }
+router.post("/", async (req, res) => {
+  const [email, password] = req.body;
+
+  //* 1- Check email
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(401).json({
+      message: "ERROR 401 - Incorrect email",
+    });
+  }
+  //* 2 - Compare user's password to hash in database
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "ERROR 401 - Incorrect password",
+    });
+  }
+
+  //* 3 - Authentification
+  // *! 3.1 - Generate a token with jsonwebtoken
+  const token = jwt.sign({ id: user._id }, secret, { expiresIn: "180s" }); // test 3 min
+  // *! 3.2 - Store token in a cookie called "jwt" and send it to client in response with a message of successful login
+  return res
+    .cookie("jwt", token, { httpOnly: true, secure: false })
+    .status(200)
+    .json({ message: "You logged in successfully" });
+});
 
 module.exports = router;
