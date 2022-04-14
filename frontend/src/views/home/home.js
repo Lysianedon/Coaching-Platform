3bd3bb024d904224a58f3f93e4bd1a3d9d95e442
anitamayousse <anita.mayousse@gmail.com>
2022-04-14 14:39:08 +0200

    added home , footer and nav components

---
+++ frontend/src/views/home/home.js
@@ -0,0 +1,14 @@
+// components
+import  Nav from '../../components/nav/nav';
+import  Footer  from '../../components/footer/footer';
+
+function Home() {
+  return (
+    <>
+      <Nav/>
+      <div>Home</div>
+      <Footer/>
+    </>
+  )
+}
+export default Home;
