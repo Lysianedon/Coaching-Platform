5c35b6293a317952bd503a84a3db83f24f7fe8e9
lysiandon <don.lysiane@gmail.com>
2022-04-16 21:28:00 +0200

    merging w/ Jess

--- backend/routers/dashboardRouter.js
+++ backend/routers/dashboardRouter.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcrypt');
 
-//------------ LIBRARIES ------------//
+//------------ LIBRARIES ------------ //
 const fs = require('fs');
 const path = require('path');
 
