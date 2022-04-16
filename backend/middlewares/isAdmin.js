85c20392a347a8b24a98d4bc7af9d83ffc09fb48
jess <jessica.elessa@gmail.com>
2022-04-16 21:40:32 +0200

    fixed merge conflicts with Lysiane

--- backend/middlewares/isAdmin.js
+++ backend/middlewares/isAdmin.js
@@ -1,16 +1,35 @@
+//---------------------- JWT ----------------//
+const jwt = require("jsonwebtoken");
+//----------------- SECRET ------------------//
+const secret = process.env.SECRET;
 //----------------- MODEL ---------------------//
 const User = require("../models/userModel.js");
-//---------------- MIDDLEWARE ----------------//
-const isAdmin = async (req, res, next) => {
-  const user = await User.findById(req.params.dashbord.admin);
 
-  if (user.isAdmin) {
-    next();
-  } else {
+// AUTHENTICATION/AUTHORIZATION - FOR ADMIN ONLY
+
+async function isAdmin(req, res, next) {
+  //Find user :
+  let user;
+  const token = req.cookies.jwt;
+  const data = jwt.verify(token, secret);
+
+  try {
+    user = await User.findById(data.id);
+    req.userId = data.id;
+    req.userRole = data.role;
+    req.verifiedUserInfos = data;
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error: "A problem happened." });
+  }
+
+  if (user.isAdmin === false) {
     return res.status(401).json({
-      message: "Unauthorized",
+      error: "Access denied. You must be an admin to access this page.",
     });
   }
-};
+
+  next();
+}
 
 module.exports = isAdmin;
--- backend/routers/loginRouter.js
+++ backend/routers/loginRouter.js
@@ -4,6 +4,8 @@ const router = express.Router();
 //--------------- AUTH ----------------//
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+//----------- MIDDLEWARE --------------//
+const auth = require("../middlewares/auth");
 
 //------------- SECRET --------------//
 const secret = process.env.SECRET;
@@ -34,7 +36,7 @@ router.post("/", async (req, res) => {
 
   //* 3 - Authentification
   // *! 3.1 - Generate a token with jsonwebtoken
-  const token = jwt.sign({ id: user._id }, secret, { expiresIn: "180s" }); // test 3 min
+  const token = jwt.sign({ id: user._id }, secret, { expiresIn: "30m" }); // test 3 min
   // *! 3.2 - Store token in a cookie called "jwt" and send it to client in response with a message of successful login
   return res
     .cookie("jwt", token, { httpOnly: true, secure: false })
@@ -43,3 +45,49 @@ router.post("/", async (req, res) => {
 });
 
 module.exports = router;
+
+// //-------------- EXPRESS ---------------//
+// const express = require("express");
+// const router = express.Router();
+// //--------------- AUTH ----------------//
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+
+// //------------- SECRET --------------//
+// const secret = process.env.SECRET;
+
+// //------------ MODEL -----------------//
+// const User = require("../models/userModel");
+
+// //------------- ROUTE ---------------//
+// router.post("/", async (req, res) => {
+//   const { email, password } = req.body;
+
+//   //* 1- Check user's email
+//   const user = await User.findOne({ email });
+//   if (!user) {
+//     return res.status(401).json({
+//       message: "Incorrect email or password",
+//     });
+//   }
+//   //* 2 - Check user's password and compare it to hash in database
+
+//   const isPasswordValid = await bcrypt.compare(password, user.password);
+
+//   if (!isPasswordValid) {
+//     return res.status(401).json({
+//       message: "Incorrect email or password",
+//     });
+//   }
+
+//   //* 3 - Authentification
+//   // *! 3.1 - Generate a token with jsonwebtoken
+//   const token = jwt.sign({ id: user._id }, secret, { expiresIn: "30m" }); // 30 min
+//   // *! 3.2 - Store token in a cookie called "jwt" and send it to client in response with a message of successful login
+//   return res
+//     .cookie("jwt", token, { httpOnly: true, secure: false })
+//     .status(200)
+//     .json({ message: "You logged in successfully" });
+// });
+
+// module.exports = router;
