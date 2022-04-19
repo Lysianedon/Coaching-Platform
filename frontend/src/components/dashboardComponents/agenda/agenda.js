05ec76370ec5c6e0f4b02643cfc5ac9aea6cc77f
lysiandon <don.lysiane@gmail.com>
2022-04-19 21:23:03 +0200

    added fetch mail

--- frontend/src/components/dashboardComponents/agenda/agenda.js
+++ frontend/src/components/dashboardComponents/agenda/agenda.js
@@ -1,11 +1,32 @@
-
+import { React, useState, useEffect } from "react";
+import axios from 'axios';
 // css
 import styled from 'styled-components';
 import "./agenda.css"
 function Agenda() {
+    const [emailUser,setEmailUser] = useState('');
+
+    const fetchEmailUser = () => {
+
+        axios.get('http://localhost:8000/dashboard/user', {withCredentials : true})
+        .then(res =>{ 
+            console.log(res.data);
+            let email = (res.data.user.email);
+            email = email.replace('@', '%40');
+            setEmailUser(email);
+            console.log('email user: ', emailUser);
+        })
+    }
+
+    useEffect(()=> {
+        fetchEmailUser();
+
+    }, [])
+
     return (
         <>
-            <iframe src="https://calendar.google.com/calendar/embed?src=pauline.gane.db%40gmail.com&ctz=Europe%2FParis"   height={300} className="agenda" ></iframe>
+        <h2>{emailUser}</h2>
+            <iframe src={`https://calendar.google.com/calendar/embed?src=${emailUser}&ctz=Europe%2FParis`}   height={300} className="agenda" ></iframe>
         </>
     )
  }
